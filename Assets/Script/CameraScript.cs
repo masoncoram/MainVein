54f361ffac995207a332c9199c24dc436e3d2cd6
Weallhavetime <yandor2@gmail.com>
2017-05-21 17:07:40 -0700

    This is a test bitch

---
+++ Assets/Script/CameraScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public float distance;
+    public float maxDistance = 2;
+    public GameObject player;
+
+
+    private float fuckThis;
+
+    private Vector3 offset;
+ 
+
+
+    void Start()
+    {
+
+       offset = transform.position - player.transform.position;
+
+    }
+
+
+    void Update()
+    {
+        
+
+        if(Mathf.Abs(player.transform.position.x - transform.position.x) > distance)
+        {
+
+            fuckThis = (Mathf.Sign(player.transform.position.x - transform.position.x) * distance - Mathf.Pow(((Mathf.Abs(player.transform.position.x - transform.position.x)-distance)/distance),2));
+            transform.position = new Vector3(transform.position.x + fuckThis, transform.position.y, transform.position.z);
+        }
+
+
+    }
+}
+//(Mathf.Abs(player.transform.position.x - transform.position.x) -      ) / maxDistance
+
+
+
+/*
+ * 
+           fuckThis = (Mathf.Sign(player.transform.position.x - transform.position.x) * distance);
+           transform.position = new Vector3(player.transform.position.x - fuckThis, transform.position.y, transform.position.z);
+ * 
+ * 
+ * 
+ * 
+ */
